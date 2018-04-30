1d5f5dcd97261782f0c2c31814a0f73abae8dbfe
GoupilJeremy <goupiljeremy@gmail.com>
2018-04-30 12:40:34 +0200

    modification des différents liens et logos

--- gatsby-generator/static-site/data/SiteConfig.js
+++ gatsby-generator/static-site/data/SiteConfig.js
@@ -3,30 +3,30 @@ module.exports = {
   lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
   siteTitle: "Raise-the-dojo", // Site title.
   siteTitleAlt: "to be defined...", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteLogo: "https://pbs.twimg.com/profile_images/826004190359060480/PhbO1qWJ_400x400.jpg", // Logo used for SEO and manifest.
   siteUrl: "https://arolla.fr", // Domain of your website without pathPrefix.
   pathPrefix: "/raise-the-dojo", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: " Ce dépôt décris ce qu'est un dojo, a quoi ca sert et pourquoi vous et vos décideurs en voulez chez vous. ", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-82450300-1 ", // GA tracking ID.
-  disqusShortname: "https-ericwindmill-github-io-gatsby-advanced-starter", // Disqus shortname.
+  disqusShortname: "", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "User", // Username to display in the author segment.
-  userTwitter: "ericwindmill", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Portland, OR", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userTwitter: "ArollaFR", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Paris, FR", // User location to display in the author segment.
+  userAvatar: "https://pbs.twimg.com/profile_images/826004190359060480/PhbO1qWJ_400x400.jpg", // User avatar to display in the author segment.
   userDescription: "All about me!", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
       label: "GitHub",
-      url: "https://github.com/ericwindmill",
+      url: "https://github.com/arolla/raise-the-dojo",
       iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
-      url: "https://twitter.com/ericwindmill",
+      url: "https://twitter.com/ArollaFr",
       iconClassName: "fa fa-twitter"
     },
     // {
@@ -39,11 +39,11 @@ module.exports = {
       url: "mailto:eric@ericwindmill.com",
       iconClassName: "fa fa-envelope"
     },
-    {
-      label: "Instagram",
-      url: "https://instagram.com/ericwindmill",
-      iconClassName: "fa fa-envelope"
-    }
+    // {
+    //   label: "Instagram",
+    //   url: "https://instagram.com/ericwindmill",
+    //   iconClassName: "fa fa-envelope"
+    // }
   ],
   copyright: "Copyright © 2018. Arolla", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
