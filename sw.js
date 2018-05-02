49e7611770fe5a04c827ddaac715462a762206e5
GoupilJeremy <goupiljeremy@gmail.com>
2018-05-02 17:15:22 +0200

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/raise-the-dojo/app-1252dcf07eeab4f4ba2a.js","0362d1281d8c7239a64daac4e9163839"],["/raise-the-dojo/commons-76b2aa19e6590585b707.js","ee73d3717fa7dd6ef5d8ceb969292533"],["/raise-the-dojo/index.html","87c6381e352e97d448276480a2033aec"],["/raise-the-dojo/manifest.json","96b641b4e890fe9ffd297ea3fbe83b94"],["/raise-the-dojo/offline-plugin-app-shell-fallback/index.html","7082d65c0036b3ae839c6da5b25542b2"]];
+var precacheConfig = [["/raise-the-dojo/app-1252dcf07eeab4f4ba2a.js","0362d1281d8c7239a64daac4e9163839"],["/raise-the-dojo/commons-7ac7bc6fb5e843026ce8.js","75a6859a2af4993469f908ce587d9c3e"],["/raise-the-dojo/index.html","5d7d7bad99e27c606edee109d35fc3cc"],["/raise-the-dojo/manifest.json","96b641b4e890fe9ffd297ea3fbe83b94"],["/raise-the-dojo/offline-plugin-app-shell-fallback/index.html","02cabbb4fd7b185a5fbfd42aa0d29b94"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
